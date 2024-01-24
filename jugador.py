e7f4f3cb32ddbdc5c80591bd9676c3c92c311148
David Iglesias Rodríguez <davi@b07.daw2d.iesteis.gal>
2024-01-24 22:43:12 +0100

    1.1

--- jugador.py
+++ jugador.py
@@ -1,30 +1,42 @@
-from ruleta import ruleta
+import random
+from ruleta import Ruleta
 
 
 class Jugador:
     nombre: str
     puntuacion: float
-    ruleta: ruleta
+    ruleta: Ruleta
 
     def __init__(self, nombre: str, puntuacion: float):
         self.nombre = nombre
         self.puntuacion = puntuacion
+        self.ruleta = Ruleta()
 
     def __str__(self):
-        return f"Bievenido {self.nombre} empiezas con {self.puntuacion}"
+        return f"Bienvenido {self.nombre}, empiezas con {self.puntuacion}"
 
-    def tirar(this):
+    def tirar(self):
+        nueva_tirada = random.choice(self.ruleta.gajos)
+        return f"La ruleta ha parado en {nueva_tirada}"
+
+    def comprar_vocal(self, vocal):
+        precio_vocal = 50.0
+        if self.puntuacion >= precio_vocal:
+            if self.ruleta.letras[vocal] == False:
+                self.ruleta.letras[vocal] = True
+                return "Se compra"
+        else:
+            return "No se compra"
         pass
 
-    def comprar_vocal():
+    def decir_letra(self):
         pass
 
-    def decir_letra():
-        pass
-
-    def resolver_panel():
+    def resolver_panel(self):
         pass
 
 
+# Crear la instancia de Jugador sin necesidad de pasar la lista de ruleta
 j1 = Jugador("Jorge", 5.0)
-print(j1)
+
+print(j1.comprar_vocal("e"))
--- ruleta.py
+++ ruleta.py
@@ -1,33 +1,35 @@
-from jugador import Jugador
-
 letras = {letra: False for letra in "abcdefghijklmnopqrstuvwxyz"}
 
 
-class ruleta:
-    jugador: Jugador
-    ruleta = [
-        "QUIEBRA",
-        "PIERDE TURNO",
-        "COMODIN",
-        "X2",
-        "/2",
-        "100",
-        "50",
-        "100",
-        "150",
-        "150",
-        "100",
-        "50",
-        "100",
-        "200",
-        "50",
-        "150",
-        "50",
-        "150",
-        "100",
-        "150",
-        "100",
-        "50",
-        "100",
-        "200",
-    ]
+class Ruleta:
+    gajos: list
+    letras: dict
+
+    def __init__(self):
+        self.abecedario = dict.fromkeys("abcdefghijklmnopqrstuvwxyz", False)
+        self.gajos = [
+            "QUIEBRA",
+            "PIERDE TURNO",
+            "COMODIN",
+            "X2",
+            "/2",
+            "100",
+            "50",
+            "100",
+            "150",
+            "150",
+            "100",
+            "50",
+            "100",
+            "200",
+            "50",
+            "150",
+            "50",
+            "150",
+            "100",
+            "150",
+            "100",
+            "50",
+            "100",
+            "200",
+        ]
